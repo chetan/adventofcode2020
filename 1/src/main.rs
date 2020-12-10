37b73088d84f23cceadcd183b0ac33b13314dd5d
Chetan Sarva <chetan@pixelcop.net>
2020-12-10 16:36:48 -0500

    day one completed

---
+++ 1/src/main.rs
@@ -0,0 +1,73 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+use std::process::exit;
+
+fn main() {
+  let args: Vec<String> = env::args().collect();
+  if &args[1] == "one" {
+    one_a();
+  } else {
+    one_b();
+  }
+}
+
+fn one_b() {
+  let ints = read_ints();
+  for a in ints {
+    for b in read_ints() {
+      for c in read_ints() {
+        if a + b + c == 2020 {
+          println!("found it! {} + {} + {} = 2020", a, b, c);
+          println!("          {} * {} * {} = {}", a, b, c, a * b * c);
+          exit(0);
+        }
+      }
+    }
+  }
+}
+
+fn read_ints() -> Vec<u32> {
+  let mut ints: Vec<u32> = Vec::new();
+  if let Ok(lines) = read_lines("./input.txt") {
+    for line in lines {
+      if let Ok(str_a) = line {
+        let a = str_a.parse::<u32>().unwrap();
+        ints.push(a);
+      }
+    }
+  }
+  return ints;
+}
+
+fn one_a() {
+  if let Ok(lines) = read_lines("./input.txt") {
+    for line in lines {
+      if let Ok(str_a) = line {
+        let a = str_a.parse::<i32>().unwrap();
+
+        if let Ok(lines_b) = read_lines("./input.txt") {
+          for line_b in lines_b {
+            if let Ok(str_b) = line_b {
+              let b = str_b.parse::<i32>().unwrap();
+              if a + b == 2020 {
+                println!("found it! {} + {} = 2020", a, b);
+                println!("          {} * {} = {}", a, b, a * b);
+                exit(0);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+  P: AsRef<Path>,
+{
+  let file = File::open(filename)?;
+  Ok(io::BufReader::new(file).lines())
+}
